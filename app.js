9a5f997663b5461879c70b8629d44ad50a0cee0d
ars0011 <68917513+ars0011@users.noreply.github.com>
2020-07-28 22:59:47 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBljYB-nFa5jbk5PhIGxQ86J_NiiqsuBoc",
+  authDomain: "hacking-34a90.firebaseapp.com",
+  databaseURL: "https://hacking-34a90.firebaseio.com",
+  projectId: "hacking-34a90",
+  storageBucket: "hacking-34a90.appspot.com",
+  messagingSenderId: "987721205665",
+  appId: "1:987721205665:web:6356872346590caa4eade0",
+  measurementId: "G-4ZJ4EM6G7X"
 };
 
      
